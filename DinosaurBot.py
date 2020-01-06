341068145fd83167153f22a155292f0b25c00f1d
Manas Asati <44468527+ManasAsati@users.noreply.github.com>
2020-01-07 00:38:55 +0530

    Add files via upload

---
+++ DinosaurBot.py
@@ -0,0 +1,39 @@
+from PIL import ImageGrab,ImageOps
+import time
+import pyautogui
+from numpy import *
+import sys
+sys.setrecursionlimit(2000)
+
+class Cordinates():
+    replayBtn = (482,460)
+    dinosaur = (187,460)
+
+def restartGame():
+    pyautogui.click(Cordinates.replayBtn)
+
+def pressSpace():
+    pyautogui.keyDown('space')
+    print("Jump")
+    pyautogui.keyUp('space')
+
+def imageGrab():
+    box = (Cordinates.dinosaur[0]+53,Cordinates.dinosaur[1]+10,Cordinates.dinosaur[0]+135,Cordinates.dinosaur[1]+45)
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    return(a.sum())
+
+def main():
+    restartGame()
+    while True:
+        if (imageGrab() !=3117):
+            pressSpace()
+        main()
+
+main()
+
+
+
+
+
